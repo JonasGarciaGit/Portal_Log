6a76a6394080f4623e7b2e6aa3a39e1ba89b9591
JonasGarciaGit <jonasemprego1@gmail.com>
2020-06-05 12:14:39 -0300

    Resolvido , textos que excedem a div

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.env = {
 
     //LOCALHOST
-    API_DOMAIN_ADDR: "http://localhost:8180/elasticOrbitall/v1/logs/esorblog-*",
+    API_DOMAIN_ADDR: "http://localhost:8080/elasticOrbitall/v1/logs/esorblog-*",
 
     //HML
     //API_DOMAIN_ADDR: "http://10.161.90.31:8080/elasticOrbitall/v1/logs/esorblog-*",
